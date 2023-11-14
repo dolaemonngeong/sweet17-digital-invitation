f1e980983f6327b695c3bff08f942e7fdcab1fae
GhifhariFebri <113836553+GhifhariFebri@users.noreply.github.com>
2023-11-14 11:51:16 +0700

    Modal RSVP

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,5 +170,26 @@
 				});
 
 		});
+		//Make Modal
+		document.addEventListener('DOMContentLoaded', function () {
+			var openModalBtn = document.getElementById('openModalBtn');
+			var closeModalBtn = document.getElementById('closeModalBtn');
+			var modal = document.getElementById('myModal');
+		  
+			openModalBtn.addEventListener('click', function () {
+			  modal.style.display = 'block';
+			});
+		  
+			closeModalBtn.addEventListener('click', function () {
+			  modal.style.display = 'none';
+			});
+		  
+			window.addEventListener('click', function (event) {
+			  if (event.target === modal) {
+				modal.style.display = 'none';
+			  }
+			});
+		  });
+		  
 
 })(jQuery);
