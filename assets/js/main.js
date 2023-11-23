a84a28b899582e70249345db9dda7045237cadb8
natanael <93458378+nextrektor@users.noreply.github.com>
2023-11-23 20:43:36 +0700

    Added Music
    Music Add for Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,18 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+const audio = new Audio();
+audio.src = "audio/bgmusic.m4a";
+audio.loop = true;
+
+function toggleAudio() {
+	if (audio.paused) {
+		audio.play();
+	} else {
+		audio.pause();
+	}
+}
+
 (function($) {
 
 	var	$window = $(window),
